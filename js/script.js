1a68d1769c8532c84dd79b95a0b9790145b24919
Melozzo <carmecamma1995@gmail.com>
2023-09-08 17:10:30 +0200

    [add]: js istructions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // 1. nome cognome chilometri da percorrere
 
-const textName = document.querySelector ('.text-name')
+const btn = document.getElementById('btn-submit');
+const inputText = document.getElementById ('text-name');
+let textName = document.getElementById('text-name');
+let ticket = document.querySelector('.text-ticket');
+
+
+btn.addEventListener('click', function(){
+  ticket.classList.remove ('d-none');
+  console.log(inputText.value);
+  const name = inputText.ariaValueMax;
+})
