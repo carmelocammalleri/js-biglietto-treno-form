b30b841c46ed5267df7affc19aa3b91c65ed657f
Melozzo <carmecamma1995@gmail.com>
2023-09-08 17:18:55 +0200

    [edit]: btn close

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // 1. nome cognome chilometri da percorrere
 
 const btn = document.getElementById('btn-submit');
+const btnClose = document.getElementById('btn-close')
 const inputText = document.getElementById ('text-name');
 let textName = document.getElementById('text-name');
 let ticket = document.querySelector('.text-ticket');
@@ -10,4 +11,8 @@ btn.addEventListener('click', function(){
   ticket.classList.remove ('d-none');
   console.log(inputText.value);
   const name = inputText.ariaValueMax;
+})
+
+btnClose.addEventListener('click', function(){
+  ticket.classList.add ('d-none');
 })
