68adbfc656857e5e97f95a78c7aa294e7bb06059
Melozzo <carmecamma1995@gmail.com>
2023-09-10 14:01:12 +0200

    [fix&remove]: fix js and remove parts superfluous

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 // 1. nome cognome chilometri da percorrere
 
 const btn = document.getElementById('btn-submit');
-const btnClose = document.getElementById('btn-close')
-const inputText = document.getElementById ('text-name');
-const travelKm = parseInt(document.getElementById('travel-km'));
-const name = inputText.ariaValueMax;
-let textName = document.getElementById('text-name');
-let ticket = document.querySelector('.text-ticket');
+const btnClose = document.getElementById('btn-close');
+
+
+
+
+
 
 
 btn.addEventListener('click', function(){
+  let ticket = document.querySelector('.text-ticket');
+
+  // NAME
+  let name = document.getElementById('text-name').value;
+  let passenger = document.getElementById('person');
+  passenger.innerHTML = name;
+
+  // TRAVEL-KM
+  let travelKm = parseInt (document.getElementById('travel-km').value);
+  let Kilometres = document.getElementById('kmetres');
+  Kilometres.innerHTML= travelKm;
+
+  // PRICE-TICKET
+  const priceKm = 0.21;
+  let priceTicket= travelKm * priceKm
+  let price = document.getElementById('prices')
+  price.innerHTML = priceTicket
+  
+  
+  console.log(priceTicket);
   ticket.classList.remove ('d-none');
-  console.log(inputText.value);
 
-})
-
-btnClose.addEventListener('click', function(){
-  ticket.classList.add ('d-none');
 })
